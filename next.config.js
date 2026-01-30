d2ff3e88cb53f8e718a6114822c0da3c96584343
conexer <conexer@gmail.com>
2026-01-29 23:18:12 -0800

    Add standalone output for Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /**@type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
@@ -8,6 +9,11 @@ const nextConfig = {
   typescript: {
     // Disable TypeScript errors during production builds
     ignoreBuildErrors: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
 }
 
